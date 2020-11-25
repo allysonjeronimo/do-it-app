afb8634ab7714251e3a57c5caff4c86e50c98dbb
Allyson Jeronimo <allysonjeronimo@gmail.com>
2020-11-25 16:37:32 -0300

    First

---
+++ app/src/main/java/com/allysonjeronimo/doit/ui/task/TaskFragment.kt
@@ -0,0 +1,31 @@
+package com.allysonjeronimo.doit.ui.task
+
+import androidx.lifecycle.ViewModelProviders
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.allysonjeronimo.doit.R
+
+class TaskFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = TaskFragment()
+    }
+
+    private lateinit var viewModel: TaskViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.task_fragment, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProviders.of(this).get(TaskViewModel::class.java)
+    }
+
+}
---
+++ app/src/main/java/com/allysonjeronimo/doit/ui/task/TaskViewModel.kt
@@ -0,0 +1,7 @@
+package com.allysonjeronimo.doit.ui.task
+
+import androidx.lifecycle.ViewModel
+
+class TaskViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
